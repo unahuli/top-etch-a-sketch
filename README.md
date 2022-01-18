0b2a5b4ccde44fa748700db68b22318ab7760e18
Sufre <gray.ratsky@gmail.com>
2022-01-18 22:36:12 +0800

    grid done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # top-etch-a-sketch
 The Odin Project etch-a-sketch project for the Foundations Path.
+
+## Pseudocode:
+**Creating the Grid** <br>
+1. Create a div container in the html file.
+2. In the JS file, query the div container as an object.
+3. Use a nested for loop to create the 16x16 grid.
--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// Creating the div container
+// Creating the grid
+const GRID_SIZE = 16;
 
+const gridPiece = document.createElement("div");
+
+gridPiece.style.width = "30px";
+gridPiece.style.backgroundColor = "black";
+gridPiece.style.height = "30px";
+gridPiece.style.border = "1px solid #C4C4C4";
+
+
+const divContainer = document.createElement("div");
+divContainer.style.width = "480px";
+//`${GRID_SIZE * gridPiece.offsetWidth}`; // gridPiece width * GRID_SIZE
+divContainer.style.height = "480px"; //gridPiece height * GRID_SIZE
+divContainer.style.display = "flex";
+divContainer.style.flexWrap = "wrap";
+// divContainer.style.justifyContent = "center";
+divContainer.id = "grid-container";
+document.body.append(divContainer);
+
+
+
+const fragment = document.createDocumentFragment();
+
+for (let row = 0; row < GRID_SIZE ** 2; row++) {
+  fragment.appendChild(gridPiece.cloneNode());
+  // for (let index = 0; index < array.length; index++) {
+  //   const element = array[index];
+    
+  // }
+}
+divContainer.append(fragment);
