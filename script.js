197b99af8dba33aa27ca917ff7d65f73cec2c4c8
Sufre <gray.ratsky@gmail.com>
2022-02-01 19:02:34 +0800

    temporarily disabled rainbow mode and added saturation filter for each pass in a square because I like to

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function createGridPiece(gridSize = 16) {
   
   gridPiece.style.flex = `1 0 ${gridPieceSize}px`;
   gridPiece.setAttribute("data-mousepass", "0");
+  console.log(gridPiece.getAttribute("data-mousepass"));
   gridPiece.classList.add("grid-piece");
 
 
@@ -76,46 +77,107 @@ function reset() {
 function setGridPieceColor(evt) {
 
   const COLOR_RANGE = 256;
-  const pass = +evt.target.getAttribute("data-mousepass");
 
-  let color = pickColor();  
-  console.log(color);
+  //rgb to hex code credits to Erick Petrucelli of StackOverflow 
+  // https://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
+  
+  // const currentColor = rgbaToHex(evt.target.style.backgroundColor);
+  // console.log(evt.target.style.backgroundColor);
+
+  const color = pickColor();  
+  // console.log(color);
   // console.log(rainbowMode);
-  if (pass === 0) {
-    if (rainbowMode) {
-      color = `rgb(${Math.random() * COLOR_RANGE}, ${Math.random() * COLOR_RANGE}, ${Math.random() * COLOR_RANGE})`;
-      evt.target.style.backgroundColor = color;
-    } else {
-      evt.target.style.backgroundColor = color;
-    }
+
+  const rainbowColorGenerator = function() {
+    color = `rgb(${Math.random() * COLOR_RANGE}, ${Math.random() * COLOR_RANGE}, ${Math.random() * COLOR_RANGE})`;
+  }
+
+  // if (currentColor !== color && !rainbowMode && pass > 0) {
+  //   evt.target.setAttribute("data-mousepass", `0`);
+  //   evt.target.style.filter = `brightness(100%)`;
+  // }
+
+  const currentColor = rgbaToHex(window.getComputedStyle(evt.target).getPropertyValue('background-color'));
+  // console.log(currentColor);
+  // console.log(typeof currentColor);
+  // console.log(rgbaToHex(window.getComputedStyle(evt.target).getPropertyValue('background-color')));
+
+  let pass = +evt.target.getAttribute("data-mousepass");
+  // let val = 0;
+  if (pass === 0 || (currentColor !== color && (!rainbowModeEnabled))){
+    evt.target.style.backgroundColor = color;
+    evt.target.style.filter = `saturate(${100}%)`
+    evt.target.setAttribute("data-mousepass", `${pass = 0}`);
+    // evt.target.style.filter = `opacity(20%)`;
+    // let blurValue = pass * 2;
+    // evt.target.style.filter = `blur(${2}px)`;
+    // evt.target.style.filter = `drop-shadow(.5rem .5rem 10rem)`;
+
   } else if (pass > 0 && pass <= 10) {
+    evt.target.style.backgroundColor = color;
     let brightnessValue = 100 - (pass * 10);
     evt.target.style.filter = `brightness(${brightnessValue}%)`;
+    let saturationValue = 100 + (pass * 15);
+    evt.target.style.filter = `saturate(${saturationValue}%)`; 
+     
+    // evt.target.setAttribute("data-mousepass", `${pass+1}`);
   }
 
   evt.target.setAttribute("data-mousepass", `${pass+1}`);
+  // if (pass === 0) {
+  //   if (rainbowMode) {
+  //     rainbowColorGenerator();
+  //     evt.target.style.backgroundColor = color;
+  //   } else {
+  //     evt.target.style.backgroundColor = color;
+  //   }
+  // } else if (pass > 0 && pass <= 10) {
+  //   let brightnessValue = 100 - (pass * 10);
+  //   // let saturationValue = 100 + (pass * 5);
+  //   evt.target.style.filter = `brightness(${brightnessValue}%)`;
+  //   // evt.target.style.filter = `saturate(${saturationValue}%)`;
+  // } 
+
+  // if (rainbowMode) {
+  //   rainbowColorGenerator();
+  //   evt.target.style.backgroundColor = color;
+  // } else {
+  //   evt.target.style.backgroundColor = color;
+  // }
+
+
+  // 
 }
 
 function pickColor() {
   const picker = document.querySelector("#color-picker");
   let color = picker.value;
   // console.log(color);
-  picker.addEventListener("input", (evt) => {
-    color = evt.target.type.value;
-  })
+  // picker.addEventListener("input", (evt) => {
+  //   color = evt.target.type.value;
+  //   rainbowMode = false;
+  // })
   return color;
 }
 
 //rainbow function
-let rainbowMode = false;
+let rainbowModeEnabled = false;
 
 const rainbowBtn = document.querySelector("#rainbow-mode");
 rainbowBtn.addEventListener("click", toggleRainbowMode);
 
 function toggleRainbowMode(){
-  rainbowMode = !rainbowMode;
+  rainbowModeEnabled = !rainbowModeEnabled;
   // console.log(rainbowMode);
 }
 
+/**
+change color of the grid part
+when you pick a new color
+reset pass to 0
+ */
 
+const rgbaToHex = (rgba) => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => 
+  (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`;
 
+  // console.log(rgbaToHex('rgb(0,0,0)'));
