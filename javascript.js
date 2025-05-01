cfabf7e1689ed36745aaf1aedb9c6272e91733fa
unahuli <40204903+unahuli@users.noreply.github.com>
2025-05-01 23:48:41 +0800

    Add button that sets the size of the grid

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 const gridContainer = document.querySelector(".grid-container");
 
 
-gridSize = 40;
-
-console.log(gridContainer.offsetHeight / gridSize);
+gridSize = 64;
 
 
+const fragment = document.createDocumentFragment();
 for (let i = 1; i <= gridSize; i++) {
     for (let j = 1; j <= gridSize; j++) {
         const gridPiece = document.createElement("div");
@@ -14,17 +13,39 @@ for (let i = 1; i <= gridSize; i++) {
         gridPiece.style.height = `${gridContainer.offsetHeight / gridSize}px`;
         gridPiece.style.backgroundColor = "black";
 
-        gridContainer.appendChild(gridPiece);
+        fragment.appendChild(gridPiece);
     }
 }
+
+gridContainer.appendChild(fragment);
 
 gridContainer.addEventListener("mouseover", (e) => {
     const id = e.target.id;
     const piece = document.querySelector(`#${id}`);
-    console.log(piece);
     piece.style.backgroundColor = "yellow";
 
 });
 
+setGridNumberBtn = document.querySelector("#set-grid-number-btn");
+console.log(setGridNumberBtn);
+
+setGridNumberBtn.addEventListener("click", (e) => {
+    gridSize = +prompt("Enter Grid Size:", 16);
+    if (gridSize > 100) gridSize = 100;
+    gridContainer.replaceChildren();
+    const fragment = document.createDocumentFragment();
+    for (let i = 1; i <= gridSize; i++) {
+        for (let j = 1; j <= gridSize; j++) {
+            const gridPiece = document.createElement("div");
+            gridPiece.id = `_${i}-${j}`;
+            gridPiece.style.width = `${gridContainer.offsetWidth / gridSize}px`;
+            gridPiece.style.height = `${gridContainer.offsetHeight / gridSize}px`;
+            gridPiece.style.backgroundColor = "black";
+    
+            fragment.appendChild(gridPiece);
+        }
+    }
+    gridContainer.appendChild(fragment);
+});
 
 
